bc518ac425a5698854171073bf938ea8c5795964
huangqijun <hqjjsw@163.com>
2014-12-14 05:15:20 -0800

    add new post pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,22 @@
 class PostsController < ApplicationController
   def home
-
+  	@post = Post.all
   end
   
   def new
 	@post = Post.new
+
   end
 
   def create
-	
+  	@post = Post.new(post_param)
+  	@post.save
+  	@post = Post.all
+  	render "home"
+  end
+
+  def post_param
+  	params.require(:posts).permit(:title, :content)
   end
 
 end
